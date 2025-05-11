d6e6df2d6a016fa506672df7c1cc0e5b13795bac
Weihon79 <ervan.chin@gmail.com>
2025-05-11 15:01:09 +0200

    optimisation du sass

---
+++ src/components/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section className="contact">
+      <h2 className="contact__title">Contact</h2>
+      <form className="contact__form">
+        <input type="text" name="lastname" placeholder="Nom" required />
+        <input type="text" name="firstname" placeholder="Prénom" required />
+        <input type="email" name="email" placeholder="Email" required />
+        <input type="text" name="subject" placeholder="Sujet" required />
+        <textarea name="message" placeholder="Message" required></textarea>
+        <button type="submit">Envoyer</button>
+      </form>
+    </section>
+  );
+};
+
+export default Contact;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../components/Header";
 import Hero from "../../components/Hero";
 import Skills from "../../components/Skills";
 import Projects from "../../components/Projects/Projects";
+import Contact from "../../components/Contact";
 import projetData from "../../data/projetData.json";
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
       <Hero />
       <Skills />
       <Projects projet={projetData} />
+      <Contact />
     </div>
   );
 };
